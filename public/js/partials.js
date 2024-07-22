278e5ec43cc60e052c637ba0a9b4f91afda6493c
Dwi Prasetyo <prasetyo1103id@gmail.com>
2024-07-22 17:15:38 +0700

    Dropdown Nav fix

--- public/js/partials.js
+++ public/js/partials.js
@@ -1,18 +1,35 @@
 // Navbar
 
+$(document).ready(function () {
+    // Toggle dropdown on profile container click
+    $("#profileContainer").click(function (event) {
+        event.stopPropagation(); // Prevent the click from bubbling up to the document
+        $("#dropdownContainer").toggle();
+    });
+
+    // Hide dropdown when clicking outside of it
+    $(document).click(function (event) {
+        if (!$(event.target).closest("#profileContainer, #dropdownContainer").length) {
+            $("#dropdownContainer").hide();
+        }
+    });
+});
+
+
+
 // Cart Sidebar
 
 $("#openCartBtn").click(function () {
     $("#cartbar").removeClass("closecart").addClass("opencart");
-    $(".overlay").css("display","block");
+    $(".overlay").css("display", "block");
 });
 
 $("#closeCartBtn").click(function () {
     $("#cartbar").removeClass("opencart").addClass("closecart");
-    $(".overlay").css("display","none");
+    $(".overlay").css("display", "none");
 });
 
-$('.overlay').click(function(){
-    $("#cartbar").removeClass("opencart").addClass("closecart");    
-    $(".overlay").css("display","none");
-})
+$(".overlay").click(function () {
+    $("#cartbar").removeClass("opencart").addClass("closecart");
+    $(".overlay").css("display", "none");
+});
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,7 +5,6 @@
             <div class="flex items-center">
                 <a href="{{ route('home') }}"
                     class="flex items-center hover:scale-110 duration-300 hover:skew-x-10 ease-in-out">
-                    <img class="h-10 w-auto" src="{{ asset('image/laravellogo.png') }}" alt="Logo" />
                     <span
                         class="hidden lg:block text-black text-3xl font-bold ml-2 after:content-['op'] after:text-blue-600">Sh</span>
                 </a>
@@ -27,9 +26,9 @@
                     </button>
                 </form>
                 <a href="{{ route('home') }}"
-                    class="h-full hidden lg:flex items-center border-b-4 border-transparent hover:border-blue-600 font-poppins hover:scale-110 duration-300 hover:skew-x-10 ease-in-out">Home</a>
+                    class="h-full hidden lg:flex items-center border-b-[4px] border-transparent hover:border-blue-600 font-poppins duration-300 hover:skew-x-10 ease-in-out">Home</a>
                 <a href="#"
-                    class="h-full flex items-center font-poppins border-b-4 border-transparent hover:border-blue-600 hover:scale-110 duration-300 hover:skew-x-10 ease-in-out">Categories</a>
+                    class="h-full flex items-center font-poppins border-b-[4px] border-transparent hover:border-blue-600 duration-300 hover:skew-x-10 ease-in-out">Categories</a>
             </div>
 
             <!-- User Info and Cart Button -->
@@ -42,33 +41,34 @@
                             d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
                 </button>
-                <div
+                <div id="profileContainer"
                     class="group relative h-full ml-5 flex items-center cursor-pointer border-b-4 border-transparent hover:border-blue-600 duration-300 ease-in-out px-3">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2"
                         stroke="currentColor" class="w-8 group-hover:text-blue-600 duration-300 ease-in-out">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                     </svg>
-                    <button class="pl-1 font-poppins group-hover:text-blue-600 duration-300 ease-in-out">
+                    <h4 class="pl-1 font-poppins group-hover:text-blue-600 duration-300 ease-in-out select-none">
                         {{ $username ?? '' }}
-                    </button>
+                    </h4>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-3 group-hover:text-blue-600 duration-300 ease-in-out">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                     </svg>
-                    <div class="absolute top-[105%] left-0 w-32 h-auto" id="dropdownContainer">
-                        <ul class="flex flex-col items-center shadow-lg py-1 bg-red-500 rounded-b-lg">
-                            <a class="w-full p-2 bg-blue-200" href="">
+                    <div class="absolute hidden top-[100%] left-[-5%] w-32 h-auto" id="dropdownContainer">
+                        <ul class="flex flex-col items-center shadow-lg border-t-4 border-blue-600 bg-blue-600 rounded-b-lg">
+                            <a class="dropdownContent w-full p-2 " href="{{route('login')}}">
                                 <li>Login</li>
                             </a>
-                            <a class="w-full p-2 bg-blue-200" href="">
+                            <a class="dropdownContent w-full p-2 " href="#">
                                 <li>My Profile</li>
                             </a>
-                            <a class="w-full p-2 bg-blue-200" href="">
-                                <li>My Profile</li>
+                            <a class="dropdownContent w-full p-2 " href="#">
+                                <li>Edit Profile</li>
                             </a>
-                            <a class="w-full p-2 bg-blue-200" href="">
-                                <li>My Profile</li>
+                            <hr>
+                            <a class="dropdownContent w-full p-2 rounded-b-lg" href="{{route('logout')}}">
+                                <li>Logout</li>
                             </a>
 
                         </ul>
