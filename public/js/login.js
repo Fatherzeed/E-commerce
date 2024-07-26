98d9c07df556dad0ab3f95e92dde02e6f8e1ba54
habibihabibi1 <bibie.ahm@gmail.com>
2024-07-26 20:52:32 +0700

    halaman login

--- public/js/login.js
+++ public/js/login.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function () {
     const $imageContainer = $("#imageContainer");
+    const $imgLogin = $("#imgLogin");
+    const $hidde = $("#hidde");
     const $registerButton = $("#registerButton");
     const $loginButton = $("#loginButton");
 
@@ -12,9 +14,18 @@ $(document).ready(function () {
     $loginButton.click(function () {
         $imageContainer.addClass("shift-right").removeClass("shift-left");
     });
+
+    $registerButton.click(function () {
+        $imgLogin.addClass("img-object-left").removeClass("img-object-right");
+    });
+
+    $loginButton.click(function () {
+        $imgLogin.addClass("img-object-right").removeClass("img-object-left");
+    });
+
+    $registerButton.click(function () {
+        $hidde.addClass("display").removeClass("display-hidden");
+    });
 });
 
 // verifikasi
-
-
-
--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -4,25 +4,30 @@
 @section('content')
     @include('partials.navbar')
     <section class="bg-white flex items-center justify-center min-h-screen ">
-        <div class="flex bg-[#eee] shadow-lg rounded-lg overflow-hidden w-2/5 max-h-[60dvh] relative items-center">
+        <div class="flex bg-[#eee] shadow-lg rounded-lg overflow-hidden w-2/5 max-h-[60dvh] relative items-center h-[600px]">
             <div id="imageContainer"
                 class="shift-right absolute w-1/2 h-full overflow-hidden transform transition-transform duration-500">
-                <img src="{{ asset('image/img-login.jpg') }}" alt="" class="w-full h-full object-cover object-left">
+                <img id="imgLogin" src="{{ asset('image/img-login.jpg') }}" alt="" class="w-full h-full object-cover img-object-right">
+
             </div>
             {{-- login --}}
             <div id="loginContainer" class="w-1/2 p-8 bg-[#eee]">
                 <h2 class="text-6xl font-bold mb-6 text-center ">Sh<span class="text-sky-600">op</span></h2>
+                
                 {{-- <form action="" method="POST">
                 @csrf --}}
+                  
                 <div class="mb-4">
                     <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
+                    <p id="error-message" class="text-xs font-poppins text-red-500"></p>
                     <input type="text" name="username" id="username" placeholder="username"
-                        class="italic mt-1 block w-full p-2 border rounded-md text-sm">
+                        class="italic mt-1 block w-full p-2 border rounded-md text-sm focus:outline-none focus:ring-1 focus:ring-sky-600">
                 </div>
                 <div class="mb-4">
                     <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                    <p id="error-message-ps" class="text-xs font-poppins text-red-500"></p>
                     <input type="password" name="password" id="password" placeholder="password"
-                        class="italic mt-1 block w-full p-2 border rounded-md text-sm">
+                        class="italic mt-1 block w-full p-2 border rounded-md text-sm  focus:outline-none focus:ring-1 focus:ring-sky-600">
                 </div>
                 <div class="mb-4 flex justify-between items-center">
                     <div>
@@ -38,31 +43,32 @@
                         class="font-bold text-blue-500 font-poppins">Sign Up here</button></p>
             </div>
             {{-- register --}}
-            <div class="w-1/2 p-8 bg-[#eee]">
+            <div id="hidde" class="w-1/2 p-8 bg-[#eee] display-hidden">
+
                 <h2 class="text-6xl font-bold mb-6 text-center ">Sh<span class="text-sky-600">op</span></h2>
                 {{-- <form action="" method="POST">
                     @csrf --}}
                     <div class="mb-4">
                         <label for="username" class="block text-sm font-medium text-gray-700" >Username</label>
                         <input type="text" name="regUsername" id="regUsername" placeholder="Username"
-                            class="mt-1 block w-full p-2 border rounded-md italic text-sm" required>
+                            class="mt-1 block w-full p-2 border rounded-md italic text-sm  focus:outline-none focus:ring-1 focus:ring-sky-600" required>
                     </div>
                     <div class="mb-4">
                         <label for="email" class="block text-sm font-medium text-gray-700" >Email</label>
                         <input type="email" name="regEmail" id="regEmail" placeholder="Email"
-                            class="mt-1 block w-full p-2 border rounded-md italic text-sm">
+                            class="mt-1 block w-full p-2 border rounded-md italic text-sm  focus:outline-none focus:ring-1 focus:ring-sky-600">
                     </div>
                     <div class="mb-4">
                         <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
                         <input type="password" name="regPassword" id="regPassword" placeholder="Password"
-                            class="mt-1 block w-full p-2 border rounded-md italic text-sm">
+                            class="mt-1 block w-full p-2 border rounded-md italic text-sm  focus:outline-none focus:ring-1 focus:ring-sky-600">
                             <div class="err"></div>
                     </div>
                     <div class="mb-4">
                         <label for="password_confirmation" class="block text-sm font-medium text-gray-700">Confirm
                             Password</label>
                         <input type="password" name="regPassword_confirmation" id="regPassword_confirmation"
-                            placeholder="Confirm Password" class="mt-1 block w-full p-2 border rounded-md italic text-sm">
+                            placeholder="Confirm Password" class="mt-1 block w-full p-2 border rounded-md italic text-sm  focus:outline-none focus:ring-1 focus:ring-sky-600">
 
                     </div>
                     <button id="btnregist" class="w-full bg-blue-500 text-white p-2 rounded-md font-poppins">Sign
@@ -79,6 +85,35 @@
 @endsection
 
 @section('script')
+
+    <script>
+        function validateInput(inputId, errorMessageId, message) {
+            const input = document.getElementById(inputId);
+            const errorMessage = document.getElementById(errorMessageId);
+            
+
+            input.addEventListener('input', function() {
+                if (input.value.trim() === '') {
+                    errorMessage.textContent = message;
+                } else {
+                    errorMessage.textContent = ''; 
+                }
+            });
+
+        
+            if (input.value.trim() === '') {
+                errorMessage.textContent = message;
+            } else {
+                errorMessage.textContent = ''; 
+            }
+        }
+
+    
+        window.addEventListener('DOMContentLoaded', () => {
+            validateInput('username', 'error-message', 'username harus diisi');
+            validateInput('password', 'error-message-ps', 'password harus diisi');
+        });
+    </script>
     <script>
         $(document).ready(function() {
             $("#btnLogin").click(function(event) {
@@ -86,14 +121,20 @@
 
                 var email = $("#username").val().trim();
                 var password = $("#password").val().trim();
+                var errorMessage = $("#error-message");
+                var errorMessagePs = $("#error-message-ps");
+                errorMessage.empty();
+                errorMessagePs.empty();
+
+                
+
                 if (!email) {
-                    showMessage("error", "Masukkan Username");
+                    errorMessage.text("masukkan username");
                     return;
                 } else if (!password) {
-                    showMessage("error", "Masukkan Password");
+                    errorMessagePs.text("masukkan password");
                     return;
                 }
-
 
                 if (/\s/.test(email)) {
                     showMessage("error", "Username tidak boleh mengandung spasi");
