41f4427f229af1c57af75cc76c16d67ce2edec28
MrPrasetyo <prasetyo1103id@gmail.com>
2024-07-26 13:56:27 +0700

    Penambahan Produk di Home

--- resources/views/sections/produk.blade.php
+++ resources/views/sections/produk.blade.php
@@ -1,44 +1,52 @@
-<section class="produk-section h-[120dvh] w-full bg-gray-100 grow-0 shrink-0">
-    <div id="collectionContainer" class="wrapper80 flex flex-col grow-0 shrink-0 px-10 py-5 h-1/2 mx-auto gap-y-4">
-        <h1 class="textJudul w-full text-center">Collection</h1>
+<section class="produk-section w-full bg-white grow-0 shrink-0">
+    <div id="collectionContainer" class="wrapper80 flex flex-col grow-0 shrink-0 px-10 py-10 mx-auto gap-y-4">
+        <h1 class="textJudul w-full text-center">Categories</h1>
         <div class="relative px-5">
-            <button id="arrow-left" class="arrow-left absolute left-0 bg-black bg-opacity-50 text-white p-2 cursor-pointer z-10">
+            <button id="arrow-left"
+                class="arrow-left absolute left-0 bg-black bg-opacity-50 text-white p-2 cursor-pointer z-10">
                 &#10094;
             </button>
-            <button id="arrow-right" class="arrow-right absolute right-0 bg-black bg-opacity-50 text-white p-2 cursor-pointer z-10">
+            <button id="arrow-right"
+                class="arrow-right absolute right-0 bg-black bg-opacity-50 text-white p-2 cursor-pointer z-10">
                 &#10095;
             </button>
-            <div id="kategoriContainer" class="kategoriContainer flex flex-row p-2 overflow-x-auto scroll-smooth snap-x scrollbar-none">
+            <div id="kategoriContainer"
+                class="kategoriContainer flex flex-row p-2 overflow-x-auto scroll-smooth snap-x scrollbar-none">
                 <!-- Kategori Items -->
-                <div class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
+                <div
+                    class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
                     <div class="relative overflow-hidden w-full h-80">
                         <img class="w-full h-80 object-cover rounded-md transform transition-transform duration-500 hover:scale-110"
                             src="{{ asset('image/product1.jpg') }}" alt="produk 1">
                     </div>
                     <h3 class="font-poppins font-semibold text-2xl mt-2">Baju Pria</h3>
                 </div>
-                <div class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
+                <div
+                    class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
                     <div class="relative overflow-hidden w-full h-80">
                         <img class="w-full h-80 object-cover rounded-md transform transition-transform duration-500 hover:scale-110"
                             src="{{ asset('image/product1.jpg') }}" alt="produk 1">
                     </div>
                     <h3 class="font-poppins font-semibold text-2xl mt-2">Baju Pria</h3>
                 </div>
-                <div class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
+                <div
+                    class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
                     <div class="relative overflow-hidden w-full h-80">
                         <img class="w-full h-80 object-cover rounded-md transform transition-transform duration-500 hover:scale-110"
                             src="{{ asset('image/product1.jpg') }}" alt="produk 1">
                     </div>
                     <h3 class="font-poppins font-semibold text-2xl mt-2">Baju Pria</h3>
                 </div>
-                <div class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
+                <div
+                    class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
                     <div class="relative overflow-hidden w-full h-80">
                         <img class="w-full h-80 object-cover rounded-md transform transition-transform duration-500 hover:scale-110"
                             src="{{ asset('image/product1.jpg') }}" alt="produk 1">
                     </div>
                     <h3 class="font-poppins font-semibold text-2xl mt-2">Baju Pria</h3>
                 </div>
-                <div class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
+                <div
+                    class="kategoriItem flex-shrink-0 snap-center w-[340px] h-100 p-4 mr-4 text-center overflow-hidden">
                     <div class="relative overflow-hidden w-full h-80">
                         <img class="w-full h-80 object-cover rounded-md transform transition-transform duration-500 hover:scale-110"
                             src="{{ asset('image/product1.jpg') }}" alt="produk 1">
@@ -51,7 +59,100 @@
         </div>
     </div>
 
-    <div class="brandNew h-1/2 bg-red-200 w-full">
+
+    <div class="brandNew bg-gray-100 w-full py-10">
+        <h1 class="textJudul w-full text-center">Brand New Items !</h1>
+        <div class="lg:max-w-[80%] w-full mx-auto grid xl:grid-cols-4 md:grid-cols-2 ">
+            <!-- Brand new Items -->
+            <div
+                class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg font-poppins">
+                <img class="h-56 w-full object-cover object-center relative"
+                    src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    alt="Product Image" />
+                    <span class="absolute text-white rounded-full p-2 top-2 right-2 tracking-thin bg-red-500">New!</span>
+                <div class="p-4 text-center">
+                    <h2 class="text-lg font-medium text-gray-900">Product Name</h2>
+                    <p class="text-lg font-semibold text-red-600">Rp 100.000</p>
+                </div>
+            </div>
+            <div
+                class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg font-poppins">
+                <img class="h-56 w-full object-cover object-center relative"
+                    src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    alt="Product Image" />
+                    <span class="absolute text-white rounded-full p-2 top-2 right-2 tracking-thin bg-red-500">New!</span>
+                <div class="p-4 text-center">
+                    <h2 class="text-lg font-medium text-gray-900">Product Name</h2>
+                    <p class="text-lg font-semibold text-red-600">Rp 100.000</p>
+                </div>
+            </div>
+            <div
+                class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg font-poppins">
+                <img class="h-56 w-full object-cover object-center relative"
+                    src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    alt="Product Image" />
+                    <span class="absolute text-white rounded-full p-2 top-2 right-2 tracking-thin bg-red-500">New!</span>
+                <div class="p-4 text-center">
+                    <h2 class="text-lg font-medium text-gray-900">Product Name</h2>
+                    <p class="text-lg font-semibold text-red-600">Rp 100.000</p>
+                </div>
+            </div>
+            <div
+                class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg font-poppins">
+                <img class="h-56 w-full object-cover object-center relative"
+                    src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    alt="Product Image" />
+                    <span class="absolute text-white rounded-full p-2 top-2 right-2 tracking-thin bg-red-500">New!</span>
+                <div class="p-4 text-center">
+                    <h2 class="text-lg font-medium text-gray-900">Product Name</h2>
+                    <p class="text-lg font-semibold text-red-600">Rp 100.000</p>
+                </div>
+            </div>
+            <div
+                class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg font-poppins">
+                <img class="h-56 w-full object-cover object-center relative"
+                    src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    alt="Product Image" />
+                    <span class="absolute text-white rounded-full p-2 top-2 right-2 tracking-thin bg-red-500">New!</span>
+                <div class="p-4 text-center">
+                    <h2 class="text-lg font-medium text-gray-900">Product Name</h2>
+                    <p class="text-lg font-semibold text-red-600">Rp 100.000</p>
+                </div>
+            </div>
+            <div
+                class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg font-poppins">
+                <img class="h-56 w-full object-cover object-center relative"
+                    src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    alt="Product Image" />
+                    <span class="absolute text-white rounded-full p-2 top-2 right-2 tracking-thin bg-red-500">New!</span>
+                <div class="p-4 text-center">
+                    <h2 class="text-lg font-medium text-gray-900">Product Name</h2>
+                    <p class="text-lg font-semibold text-red-600">Rp 100.000</p>
+                </div>
+            </div>
+            <div
+                class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg font-poppins">
+                <img class="h-56 w-full object-cover object-center relative"
+                    src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    alt="Product Image" />
+                    <span class="absolute text-white rounded-full p-2 top-2 right-2 tracking-thin bg-red-500">New!</span>
+                <div class="p-4 text-center">
+                    <h2 class="text-lg font-medium text-gray-900">Product Name</h2>
+                    <p class="text-lg font-semibold text-red-600">Rp 100.000</p>
+                </div>
+            </div>
+            <div
+                class="mx-auto mt-11 w-80 transform overflow-hidden rounded-lg bg-white shadow-md duration-300 hover:scale-105 hover:shadow-lg font-poppins">
+                <img class="h-56 w-full object-cover object-center relative"
+                    src="https://images.unsplash.com/photo-1674296115670-8f0e92b1fddb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                    alt="Product Image" />
+                    <span class="absolute text-white rounded-full p-2 top-2 right-2 tracking-thin bg-red-500">New!</span>
+                <div class="p-4 text-center">
+                    <h2 class="text-lg font-medium text-gray-900">Product Name</h2>
+                    <p class="text-lg font-semibold text-red-600">Rp 100.000</p>
+                </div>
+            </div>
+        </div>
 
     </div>
 </section>
