35985f6de8b78b079b057e48901f56e2ef65c35b
habibihabibi1 <bibie.ahm@gmail.com>
2024-08-19 18:12:48 +0700

    penyesuaian sedikit

--- resources/views/sections/kategori-produk.blade.php
+++ resources/views/sections/kategori-produk.blade.php
@@ -1,75 +1,75 @@
 <section class="bg-slate-300">
     <div class="wrapper90 items-center mx-auto mt-5 grid gap-x-[60px] grid-cols-5 py-16"> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-    <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white">
-        <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
-        <div class="py-4 m-2">
-            <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
-            <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
-        </div>
-    </div> 
-</div>
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+        <div class="shadow-lg overflow-hidden xl:w-[227px] mb-5 bg-white duration-300 hover:scale-105 hover:shadow-lg hover:rounded ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full h-[174px]">
+            <div class="py-4 m-2">
+                <h1 class="font-poppins text-sm">CELANA CARGO PANJANG PALING MURAH DAN KEREN </h1>
+                <h2 class="font-poppins text-sm mt-3 text-red-600">Rp37.620</h2>
+            </div>
+        </div> 
+    </div>
 
 </section>
--- resources/views/sections/kategori.blade.php
+++ resources/views/sections/kategori.blade.php
@@ -2,13 +2,13 @@
 
     <div class="mx-auto px-10 pt-4 flex flex-wrap gap-4 justify-evenly shrink-0 mt-0 ">
         <a href="/categori" class="rounded-md   shadow-lg border mb-10 xl:w-[426px] xl:h-[564px]">
-            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full xl:h-[400px] p-5 pt-8 ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full xl:h-[400px] p-5 hover:rounded-3xl transform transition-transform duration-500 hover:scale-110 ">
             <div class="py-10">
                 <div class="font-poppins font-bold text-center text-[32px]">Pakaian Pria</div>
             </div>
         </a>
         <a href="/categori" class="rounded-md   shadow-lg border mb-10 xl:w-[426px] xl:h-[564px]">
-            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full xl:h-[400px] p-5 pt-8 ">
+            <img src="{{ asset('image/product1.jpg') }}" alt="For You" class="w-full xl:h-[400px] p-5 hover:rounded-3xl transform transition-transform duration-500 hover:scale-110 ">
             <div class="py-10">
                 <div class="font-poppins font-bold text-center text-[32px]">Pakaian Wanita</div>
             </div>
